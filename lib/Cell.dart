412b761e7ade3f975a425bfcc35eac0e1f45e366
TheYautja <am.96138669@gmail.com>
2026-03-13 20:16:13 -0300

    add texturas pra grid

--- lib/Cell.dart
+++ lib/Cell.dart
@@ -1,16 +1,30 @@
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+import 'dart:ui';
 
-class Cell {
+class Cell extends PositionComponent {
 
-  int? x;
-  int? y;
+  final int cellX;
+  final int cellY;
   bool isShip = false;
   bool discovered = false;
-  //add textura dps
+  final Sprite texture;
 
-  Cell(int x, int y, bool isShip){
-    this.x = x;
-    this.y = y;
-    this.isShip = false;
+
+  static const double tilesize = 32.0;
+
+
+  Cell(this.cellX, this.cellY, this.isShip, this.texture){
+    position = Vector2(cellX * tilesize, cellY * tilesize);
+    size = Vector2.all(tilesize);
   }
+
+
+  @override
+  void render(Canvas canvas){
+    super.render(canvas);
+    texture.render(canvas, size: size);
+  }
+
 
 }
--- lib/battleship.dart
+++ lib/battleship.dart
@@ -1,11 +1,15 @@
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/sprite.dart';
 import 'Cell.dart';
 
 class Battleship extends FlameGame {
 
-  int gridWidth = 1;
-  int gridHeight = 1;
+  int gridWidth = 16;
+  int gridHeight = 16;
 
+  List<List<Cell>> grid = [];
 
   Battleship(int gridWidth, int gridHeight){
     this.gridWidth = gridWidth;
@@ -15,16 +19,34 @@ class Battleship extends FlameGame {
 
   @override
   Future<void> onLoad() async {
-    print("teeeeste batalhanaval");
+    await load_sprites();
+    generate_grid();
+  }
+
+
+  Future<void> load_sprites() async {
+    await Flame.images.load('water.png');
+  }
+
+
+  Sprite get_sheet_frag(double x, double y, double width, double height){
+    return Sprite(
+      Flame.images.fromCache('water.png'),
+      srcPosition: Vector2(x, y),
+      srcSize: Vector2(width, height),
+    );
   }
 
 
   void generate_grid(){
-    List<List<Cell>> grid = [];
     for(int i = 0; i < gridWidth; i++){
+      List<Cell> row = [];
       for(int j = 0; j < gridHeight; j++){
-        grid[i][j] = Cell(i, j, false);
+        final cell = Cell(i, j, false, get_sheet_frag(70.0, 30.0, 32.0, 32.0));
+        row.add(cell);
+        add(cell);
       }
+      grid.add(row);
     }
   }
 
